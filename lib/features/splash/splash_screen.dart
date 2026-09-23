54393959543d367058863de7f7392e2b83e46f54
habibamahmoud-design <enghabibamahmoud@gmail.com>
2026-09-23 15:12:11 +0300

    Update splash  screen

--- lib/features/splash/splash_screen.dart
+++ lib/features/splash/splash_screen.dart
@@ -27,6 +27,7 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xFFF5F7FB),
       body: Center(child: Lottie.asset('assets/icons/splash.json')),
     );
   }
